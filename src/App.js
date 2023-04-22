2f98ad8873e82356424ca5a7ecb9441bffbdd30b
Abhishree <abhishree881@gmail.com>
2023-04-22 09:46:49 +0530

    created project structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import { useState } from "react";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+
 function App() {
-  return <div className="App">Podcasts</div>;
+  const [currentUser, setCurrentUser] = useState("");
+  const ProtectedRoute = () => {
+    if (!currentUser) {
+      return <Navigate to="/signup" />;
+    }
+  };
+  return (
+    <div className="App">
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
