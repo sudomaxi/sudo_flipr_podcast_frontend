2a55909b3224f71a5488e1493cbab1ece6f87af8
Abhishree <abhishree881@gmail.com>
2023-04-22 12:20:52 +0530

    made admin page

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,5 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Add from "../assets/add.png";
 
 export default function Admin() {
-  return <div>Admin</div>;
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Podcasts</span>
+        <span className="title"></span>
+        <form>
+          <input required style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>Add Thumbnail</span>
+          </label>
+          <input required type="text" placeholder="Title for Podcast" />
+          <input required type="text" placeholder="Artist" />
+          <input required type="text" placeholder="Tags" />
+          <input required style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>Add audio</span>
+          </label>
+          <button>Add Podcast</button>
+          {/* {loading && "Uploading and compressing the image please wait..."}
+          {err && <span>Something went wrong</span>} */}
+        </form>
+      </div>
+    </div>
+  );
 }
